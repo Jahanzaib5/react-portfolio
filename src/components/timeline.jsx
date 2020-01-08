e48946e51b385993232d9e311e6123d4fee6be75
Jahanzaib5 <50393893+Jahanzaib5@users.noreply.github.com>
2020-01-08 19:54:59 +0600

    in process

--- src/components/timeline.jsx
+++ src/components/timeline.jsx
@@ -21,8 +21,12 @@ export default class Timeline extends Component {
                         <i className="icon-pen2" />
                       </div>
                       <div className="timeline-label">
-                        <h2>Internship at Juniper <span>2019-present</span></h2>
-                        <p>I recently have joined the Juniper Networs as a Software Developer-Intern in the DevOps team. My major part of the work has been into the field of creating sustainable and flexible CI/CD (Continuous Integration & Continuous Development) pipelines which enables developers at Juniper to do rapid development. I am also exploring the role of container technology like Docker and container-orchrastation technologies like K8s, Swarm to create industry standard production environments.</p>
+                        <h2>Undergradute student at UCA, Naryn Campus <span>2016-2021(expectd)</span></h2>
+                        <p>I am a junior year student at University of Central Asia, majoring in Computer Science.
+                          We have been following the syllabus and courses designed by the University of Toroto for us,
+                          which complies with the modern world requirements. The courses are desinged in way that we are not
+                          just learning new things, but also trying to imply that knowledge to our daily life activities.
+                        </p>
                       </div>
                     </div>
                   </article>
@@ -32,7 +36,7 @@ export default class Timeline extends Component {
                         <i className="icon-pen2" />
                       </div>
                       <div className="timeline-label">
-                        <h2>Undergraduation at DAIICT<span>2015-2019</span></h2>
+                        <h2>Internship at Uexel, Islamabad, Pakistan <span>may-2019-july-2019</span></h2>
                         <p>I am pursuing my under-graduation studies with major in I.C.T.(Information & Communication Technology). I have taken courses like DSA, OOPs, Computer Networks, IOT, Web-Data Management over the years and have better understanding of these subjects.I have also been part of S.B.G.(Student Body Govenance) scince my first year of college.</p>
                       </div>
                     </div>
